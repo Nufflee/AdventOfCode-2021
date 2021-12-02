b42cd4d3f291c117b0b8834adebff452aec61b5b
Nufflee <zifcani@gmail.com>
2021-12-02 18:53:42 +0100

    [Day 1] Rename from `day1` to `day01`

--- day01/src/part2.rs
+++ day01/src/part2.rs
@@ -1,6 +1,7 @@
 pub fn count_increases(depths: &[u32]) -> u32 {
     let mut increases = 0u32;
 
+    // TODO: Try doing some intellectual masturbation here using zips 'n stuff
     for i in 0..depths.len() - 3 {
         let sum1: u32 = depths[i..i + 3].iter().sum();
         let sum2: u32 = depths[i + 1..i + 4].iter().sum();
